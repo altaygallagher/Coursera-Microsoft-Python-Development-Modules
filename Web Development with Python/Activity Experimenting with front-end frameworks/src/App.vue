abe28063920d4b8863908c98e6e526222ef20378
Altay YELES <altay.yeles@std.yildiz.edu.tr>
2025-08-19 04:45:50 +0300

    add files via upload

---
+++ Web Development with Python/Activity Experimenting with front-end frameworks/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+/* To use a Vue component, it can be imported in the script area of the App.vue file like: */
+/* import ComponentName from './components/ComponentName.vue' */
+import Heading from './components/Heading.vue'
+import Copyright from './components/Copyright.vue'
+import Information from './components/Information.vue'
+</script>
+
+<template>
+  <h1>Coursera Vue.js Demo</h1>
+  <!-- The components are used in the template area of the App.vue file. -->
+  <!-- To display a Vue component, the name of the component is treated similar to an HTML tag. -->
+   <p>
+    This is a demo application for the Coursera Vue.js course.
+  </p>
+  <Heading />
+  <Information />
+  <Copyright />
+</template>
+
+<style>
+  /* The body will have a background of antique white and a mid-black font. */
+  body {
+    color: #242424;
+    background-color: antiquewhite;
+  }
+</style>
